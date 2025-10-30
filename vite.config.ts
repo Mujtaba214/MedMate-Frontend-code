fab8f32631413db0c41c560c882d6784350e7238
Muhammad Mujtaba <mujtabausman21@gmail.com>
2025-10-31 02:36:08 +0500

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // ✅ important for Netlify or relative deployments
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
